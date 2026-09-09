978526593f32021196518e5163bcd10205d74a5f
Valentin Latchimy <valentin.latchimy@outlook.fr>
2026-09-09 21:54:53 +0200

    Creation of the computerPlay and playRound functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function computerPlay() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.trim().toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+             playerSelection === 'paper' && computerSelection === 'rock' ||
+             playerSelection === 'scissors' && computerSelection === 'paper') {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
